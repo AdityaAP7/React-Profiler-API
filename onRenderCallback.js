95d0527246b2bbbf440896d1168dec70e595a4e8
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-02-10 21:35:39 +0530

    Update onRenderCallback.js

--- onRenderCallback.js
+++ onRenderCallback.js
@@ -2,3 +2,4 @@ import React, { Profiler } from 'react';
 function onRenderCallback(
   id, // the "id" prop of the Profiler tree
   phase, // either "mount" or "update"
+  actualDuration, // time spent rendering the Profiler and its descendants
