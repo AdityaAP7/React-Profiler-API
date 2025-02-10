37c00787df9ef6124f247d59f6e9501796cc3429
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-02-10 21:38:01 +0530

    Update PerformanceProvider.js

--- PerformanceProvider.js
+++ PerformanceProvider.js
@@ -1,3 +1,4 @@
 import React, { createContext, useState, useContext, useCallback } from 'react';
 const PerformanceContext = createContext();
 export function PerformanceProvider({ children }) {
+  const [metrics, setMetrics] = useState([]);
