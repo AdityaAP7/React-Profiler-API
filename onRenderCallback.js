c0dbcc8a0f5aac73b2d5c01f078e5bdb97820759
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-02-10 21:35:45 +0530

    Update onRenderCallback.js

--- onRenderCallback.js
+++ onRenderCallback.js
@@ -3,3 +3,4 @@ function onRenderCallback(
   id, // the "id" prop of the Profiler tree
   phase, // either "mount" or "update"
   actualDuration, // time spent rendering the Profiler and its descendants
+  baseDuration, // estimated time to render the entire subtree without memoization
