72b9280c80667dd445aad2d38497cd34b89ac7aa
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-02-11 23:08:38 +0530

    Update PerformanceProvider.js

--- PerformanceProvider.js
+++ PerformanceProvider.js
@@ -4,3 +4,4 @@ export function PerformanceProvider({ children }) {
   const [metrics, setMetrics] = useState([]);
   const onRenderCallback = useCallback(
     (id, phase, actualDuration, baseDuration, startTime, commitTime, interactions) => {
+      setMetrics(prev => [
