1fb09758c89eb094be6ebf767ff9a3935eea2637
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-02-11 23:13:25 +0530

    Update PerformanceDashboard.js

--- PerformanceDashboard.js
+++ PerformanceDashboard.js
@@ -7,3 +7,4 @@ function PerformanceDashboard() {
   return (
     <div style={{ position: 'fixed', bottom: 0, right: 0, width: '300px', background: '#fff' }}>
       {/* Use a chart library to plot metrics */}
+      <h4>Last Render Times</h4>
