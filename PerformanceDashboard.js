8d17a054966b4e11491ea09e998e4a427f407945
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-02-11 23:12:46 +0530

    Update PerformanceDashboard.js

--- PerformanceDashboard.js
+++ PerformanceDashboard.js
@@ -2,3 +2,4 @@
 import { usePerformanceData } from './PerformanceProvider';
 function PerformanceDashboard() {
   const { metrics } = usePerformanceData();
+  // transform data to a chart-friendly format
