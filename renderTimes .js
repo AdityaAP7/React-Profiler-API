30b5586eefdd497d07eb703b907ee31ece08d846
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-02-12 23:59:00 +0530

    Update renderTimes .js

--- renderTimes .js
+++ renderTimes .js
@@ -6,3 +6,4 @@ function onRenderCallback(id, phase, actualDuration, baseDuration) {
   renderTimes[id].push(actualDuration);
   // get average
   const sum = renderTimes[id].reduce((acc, val) => acc + val, 0);
+  const avg = sum / renderTimes[id].length;
