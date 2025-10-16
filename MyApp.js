264210194d2d16ae07a0783a92fce4f29cd51f65
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-10-16 21:21:34 +0530

    Update MyApp.js

--- MyApp.js
+++ MyApp.js
@@ -1,4 +1,4 @@
-543210987654321function MyApp() {
+6543210987654321function MyApp() {
   const { onRenderCallback } = usePerformanceData();
   return (
     <Profiler id="MyAppProfiler" onRender={onRenderCallback}>
